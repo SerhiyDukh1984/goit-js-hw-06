3f33ed38851a7138d4201b24aebffb84548f51d8
SerhiyDukh1984 <s.dux2011@ukr.net>
2022-06-23 20:50:36 +0300

    виконує завдання 2

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,21 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const createItem = function (...ingredients) {
+  const itemEl = document.createElement("li");
+  itemEl.classList.add("item");
+  itemEl.textContent = ingredients;
+  return itemEl;
+};
+
+const ingredientsList = [...ingredients].map((item) => createItem(item));
+
+const allIngredients = document.querySelector("#ingredients");
+
+allIngredients.append(...ingredientsList);
