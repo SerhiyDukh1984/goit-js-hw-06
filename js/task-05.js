50589294b48816fa2ad217813b0a8dcbbc7815b7
SerhiyDukh1984 <s.dux2011@ukr.net>
2022-06-24 22:24:53 +0300

    Update task-05.js

--- js/task-05.js
+++ js/task-05.js
@@ -1,13 +1,10 @@
 const refs = {
   input: document.querySelector("#name-input"),
-  name: document.querySelector("#name-output"),
+  nameLable: document.querySelector("#name-output"),
 };
 
 refs.input.addEventListener("input", onInputChange);
 
 function onInputChange(event) {
-  if (input !== "") {
-    refs.name.textContent = event.currentTarget.value;
-  }
-  name.textContent = "Anonymous";
+  refs.nameLable.textContent = event.currentTarget.value;
 }
