484944d2e1e9b315016c2606642cffaa052f27e2
SerhiyDukh1984 <s.dux2011@ukr.net>
2022-06-25 19:23:13 +0300

    add submit function

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,19 @@
+const formEl = document.querySelector(".login-form");
+const EmailEl = document.querySelector(".email");
+const PasswordEl = document.querySelector(".password");
+const buttonEl = document.querySelector("button");
 
+//*! event.preventDefault();
+
+buttonEl.addEventListener("click", onButtonClick);
+
+function onButtonClick(event) {
+  event.preventDefault();
+  if (EmailEl.textContent === "" || PasswordEl.textContent === "") {
+    alert("!!! ALARM !!! Всі поля мають бути заповнені!");
+  }
+  return {
+    email: `${EmailEl.textContent}`,
+    password: `${PasswordEl.textContent}`,
+  };
+}
