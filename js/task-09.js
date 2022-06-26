f4b224d63798b187fbb80ade4b43b3831ec78b2e
SerhiyDukh1984 <s.dux2011@ukr.net>
2022-06-26 13:03:54 +0300

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
+const buttonEl = document.querySelector(".change-color");
+const textEl = document.querySelector(".color");
+document.body.classList.add("backgroundColor");
+
+buttonEl.addEventListener("click", onButtonClick);
+
+function onButtonClick() {
+  document.body.style.backgroundColor = getRandomHexColor();
+  textEl.textContent = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
