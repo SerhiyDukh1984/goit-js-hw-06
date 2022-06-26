21a3237f6ee3d250af65903f2df94b507c43ffdc
SerhiyDukh1984 <s.dux2011@ukr.net>
2022-06-26 22:27:41 +0300

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -6,7 +6,17 @@ inputEl.addEventListener("blur", makeInputValidation);
 function makeInputValidation(event) {
   if (`${event.currentTarget.value.length}` !== minLength) {
     inputEl.classList.add("invalid");
-  } else {
-    inputEl.classList.replace("invalid", "valid");
+    inputEl.classList.remove("valid");
+    console.log(
+      "🚀 ~ file: task-06.js ~ line 10 ~ makeInputValidation ~ inputEl",
+      inputEl
+    );
+  } else if (`${event.currentTarget.value.length}` === minLength) {
+    inputEl.classList.add("valid");
+    inputEl.classList.remove("invalid");
+    console.log(
+      "🚀 ~ file: task-06.js ~ line 14 ~ makeInputValidation ~ inputEl",
+      inputEl
+    );
   }
 }
