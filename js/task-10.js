3cc04469ed73fec5ba91a3c05475d9435921d821
SerhiyDukh1984 <s.dux2011@ukr.net>
2022-06-27 17:24:15 +0300

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,14 @@
+const boxesEl = document.querySelector("boxes");
+const btnCreate = document.querySelector("[data-create");
+const btnDestroy = document.querySelector("[data-destroy");
+const inputEl = document.querySelector("input");
+
+function createBoxes(amount) {
+  document.createElement("div");
+}
+
+function randomColor() {}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
